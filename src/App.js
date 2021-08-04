166060ddad0bc12ace2f48c0740aa4a1577037f5
Ming Xiong <iona.m.xiong@gmail.com>
2021-08-04 23:24:49 +0300

    blog attributes & sample blogs

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
+// import axios from 'axios';
 
-const App = () => {
-  return <>blogs</>;
+const App = (props) => {
+  // const [blogs, setBlogs] = useState([
+  //   { title: '', date: '', author: '', content: '', views: 0, likes: 0 },
+  // ]);
+  console.log(props)
+
+  return (
+    <div>
+      <h1>Blogs</h1>
+      <ul>
+        {props.blogs.map((blog) => 
+          <>
+            <li index={blog.id}>
+              {blog.title}
+              {blog.date}
+            </li>
+            <li> {blog.author} </li>
+            <li> {blog.content} </li>
+            <li>
+              {' '}
+              {blog.views} {blog.likes}{' '}
+            </li>
+          </>
+        )}
+      </ul>
+    </div>
+  );
 };
 
 export default App;
